035d8a2e2c0ea1918906af1bd5e95b121334f63c
Anthony Kozar <akozar@users.sourceforge.net>
2023-04-01 20:51:31 -0400

    Add panes to PolyominoTable windows for selecting a polyomino and a color.

--- PolyTable/src/PolyominoTable.java
+++ PolyTable/src/PolyominoTable.java
@@ -1,27 +1,52 @@
-import java.awt.BorderLayout;
+/*	PolyominoTable.java
+	
+	A window that displays a grid and controls for placing polyominoes on the grid.
+	
+	Anthony Kozar
+	April 1, 2023
+	
+ */
 
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.ScrollPaneConstants;
+import	java.awt.*;
+import	java.awt.event.*;
+import	javax.swing.*;
+import	javax.swing.event.*;
 
 
 @SuppressWarnings("serial")
 public class PolyominoTable extends JFrame
 {
 	private final static String	defaultWindowName = "New Table";
-	private final static int	defaultWindowWidth = 525;
-	private final static int	defaultWindowHeight = 550;
+	private final static int	defaultWindowWidth = 650;
+	private final static int	defaultWindowHeight = 575;
+	
+	private final static String[]  polyominoNames = {"Monomino", "Domino", "I Tromino", "Elbow Tromino",
+										"I Tetromino", "L Tetromino", "Square Tetromino", "T Tetromino",
+										"S Tetromino"};
 
 	public PolyominoTable()
 	{
 		super(defaultWindowName);
 		
+		// center pane contains the scrollable grid
 		InteractiveSquareGrid grid = new InteractiveSquareGrid();
-		JScrollPane scroller = new JScrollPane(grid, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+		JScrollPane centerpane = new JScrollPane(grid, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
+		
+		// right pane contains a scrollable list of polyominoes
+		JList<String> polylist = new JList<String>(polyominoNames);
+		polylist.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		JScrollPane rightpane = new JScrollPane(polylist, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		
+		// top pane will contain buttons for colors (and edit modes?)
+		JPanel toppane = new JPanel();
+		toppane.setLayout(new FlowLayout());
+		
 		this.setSize(defaultWindowWidth, defaultWindowHeight);
-		this.add(scroller, BorderLayout.CENTER);
+		this.add(centerpane, BorderLayout.CENTER);
+		this.add(rightpane, BorderLayout.EAST);
+		this.add(toppane, BorderLayout.NORTH);
 		this.setVisible(true);
 		this.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
 		grid.requestFocusInWindow();
